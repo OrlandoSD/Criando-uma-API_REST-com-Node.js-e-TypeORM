951d953bb19c98a7866a79726e93b3f81216f969
ORLANDO DOS SANTOS DOURADO <62121619+OrlandoSD@users.noreply.github.com>
2022-07-14 10:30:53 -0300

    atualizando ' index.ts'

--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
 
 const server = express();
+
+server.get('/', (request: Request, response: Response) =>{
+   return response.json({mensagem: 'Bem vindo a nossa API'})
+})
 
 server.listen(5000, () =>{
     console.log('Servidor on na porte 5000')
